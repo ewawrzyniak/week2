3bf437246d8a71d97f7edc9acf992324404f62a6
Wawrzyniak <ewawrzyniak@cps.k12.il.us>
2024-09-04 14:59:48 -0500

    first commit

---
+++ python_basics/lesson.py
@@ -0,0 +1,28 @@
+#mkdr means make directory
+#cd means change directory
+#ls means list directory
+#pwd means print working directory
+#touch menas create a file
+#rm menas remove a file 
+# pound sign means comment 
+# print ("Hello, World!")
+# print ("Emily Wawrzyniak")
+#/t means tab 
+#press control ? to comment out multiple lines 
+#ask for input 
+name= input ("What is your name?")
+print ("Hello, " +name)
+
+age= input ("How old are you?")
+print ("You are " +age +" years old")
+
+placeofbirth= input ("Where were you born?")
+print ("You were born in " +placeofbirth)
+ 
+adress= input ("What is your adress?")
+print ("You live at " +adress)
+
+school= input ("What school did you go to?")
+print ("You went to " +school)
+
+print("your name is " +name +" and you are " +age+" years old. You were born in " +placeofbirth+ " and you currently live at " +adress+ " and attend "+ school)
---
+++ week2_python/wee2.py
@@ -0,0 +1,10 @@
+#ask someone what day iot is 
+day =input ("What day is it?")
+print ("the day is " + day) 
+#ask them for their favorite holiday
+
+holiday= input ("What is your fabvorite holiday?")
+print ("Today is " + day + " and your favorite holiday is " + holiday  )
+#print out the day and their favorite holiday in a sentence
+
+#making a change to the file. 
