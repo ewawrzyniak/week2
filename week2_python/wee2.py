1b282fa5fd67cf9b46fe4f1c487f49e8c8cac3bc
Wawrzyniak <ewawrzyniak@cps.k12.il.us>
2024-09-04 15:06:14 -0500

    string slicing

--- week2_python/wee2.py
+++ week2_python/wee2.py
@@ -1,10 +1,13 @@
-#ask someone what day iot is 
-day =input ("What day is it?")
-print ("the day is " + day) 
-#ask them for their favorite holiday
+# #ask someone what day iot is 
+# day =input ("What day is it?")
+# print ("the day is " + day) 
+# #ask them for their favorite holiday
 
-holiday= input ("What is your fabvorite holiday?")
-print ("Today is " + day + " and your favorite holiday is " + holiday  )
-#print out the day and their favorite holiday in a sentence
+# holiday= input ("What is your fabvorite holiday?")
+# print ("Today is " + day + " and your favorite holiday is " + holiday  )
+# #print out the day and their favorite holiday in a sentence
 
 #making a change to the file. 
+#string alicing 
+word= "hello" 
+print (word[1:4])
